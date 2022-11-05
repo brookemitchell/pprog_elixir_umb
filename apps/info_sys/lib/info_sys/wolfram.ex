1bd435823355215ed4e5f75cee1f2e3fabed969a
Brooke Mitchell <927600+brookemitchell@users.noreply.github.com>
2022-11-05 22:34:06 +1300

    send asyc wolfram query

--- apps/info_sys/lib/info_sys/wolfram.ex
+++ apps/info_sys/lib/info_sys/wolfram.ex
@@ -1,5 +1,5 @@
 defmodule InfoSys.Wolfram do
-  import SweetXML
+  import SweetXml
   alias InfoSys.Result
 
   @behaviour InfoSys.Backend
@@ -8,4 +8,31 @@ defmodule InfoSys.Wolfram do
 
   @impl true
   def name, do: "wolfram"
+
+  @impl true
+  def compute(query_str, _opts) do
+    query_str
+    |> fetch_xml()
+    |> xpath(~x"/queryresult/pod[contains(@title, 'Result') or
+                                 contains(@title, 'Definitions')]
+                                 /subpod/plaintext/text()")
+    |> build_results()
+  end
+
+  defp build_results(nil), do: []
+
+  defp build_results(answer),
+    do: [%Result{backend: __MODULE__, score: 95, text: to_string(answer)}]
+
+  defp fetch_xml(query) do
+    {:ok, {_, _, body}} = :httpc.request(String.to_charlist(url(query)))
+    body
+  end
+
+  defp url(input) do
+    "#{@base}?" <>
+      URI.encode_query(appid: id(), input: input, format: "plaintext")
+  end
+
+  defp id, do: Application.fetch_env!(:info_sys, :wolfram)[:app_id]
 end
